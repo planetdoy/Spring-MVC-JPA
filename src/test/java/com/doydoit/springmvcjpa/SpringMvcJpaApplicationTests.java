b508ccb2de96fba72830e9aa044631d2b59a6fd8
planetdoy <doydoit@gmail.com>
2021-09-06 21:54:53 +0900

    add project

---
+++ src/test/java/com/doydoit/springmvcjpa/SpringMvcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doydoit.springmvcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
