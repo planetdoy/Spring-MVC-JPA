5f96b20ecf0549307e4ea0f1d0557c0680bb6974
planetdoy <doydoit@gmail.com>
2021-09-08 17:46:02 +0900

    feat : db에 미리 값 저장

---
+++ src/main/java/com/doydoit/springmvcjpa/domain/InitDb.java
@@ -0,0 +1,60 @@
+package com.doydoit.springmvcjpa.domain;
+
+import com.doydoit.springmvcjpa.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+/**
+ * 총 주문 2개
+ * userA
+ *  JPA1 BOOK
+ *  JPA2 BOOK
+ * userB
+ *  SPRING1 BOOK
+ *  SPRING2 BOOK
+ */
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit1() {
+
+            String name = "item1";
+            int price = 10000;
+            int stockQuantity = 100;
+
+            Item item = new Item(name, price, stockQuantity);
+
+            em.persist(item);
+        }
+
+        public void dbInit2() {
+
+            String name = "item2";
+            int price = 20000;
+            int stockQuantity = 10;
+
+            Item item = new Item(name, price, stockQuantity);
+
+            em.persist(item);
+        }
+    }
+}
