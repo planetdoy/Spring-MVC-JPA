d9ef49d46fbd0be0ab221f8f6dfaa877475b4aed
planetdoy <doydoit@gmail.com>
2021-09-10 18:54:46 +0900

    refactor : error message code 파라미터 수정
    > errors.properties 추가

--- src/main/java/com/doydoit/springmvcjpa/domain/item/ItemController.java
+++ src/main/java/com/doydoit/springmvcjpa/domain/item/ItemController.java
@@ -46,21 +46,21 @@ public class ItemController {
     public String add(@ModelAttribute("item") ItemForm item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item", "itemName",item.getItemName(),false, null,null, "상품 명은 필수입니다."));
+            bindingResult.addError(new FieldError("item", "itemName",item.getItemName(),false, new String[]{"require.item.itemName"},null, null));
         }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >1000000) {
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(),false, null,null, "상품 가격은 1,000원 이상 1,000,000원 미만이어야 합니다."));
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(),false, new String[]{"range.item.price"},new Object[]{1000,1000000}, null));
         }
 
         if (item.getStockQuantity() == null || item.getStockQuantity() > 9999) {
-            bindingResult.addError(new FieldError("item", "stockQuantity",item.getStockQuantity(),false,null,null, "수량은 최대 9,999까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "stockQuantity",item.getStockQuantity(),false,new String[]{"max.item.quantity"},new Object[]{9999}, null));
         }
 
         if (item.getPrice() != null && item.getStockQuantity() != null) {
             int resultPrice = item.getPrice() * item.getStockQuantity();
             if (resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item",null,null, "가격 * 수량은 10,000원 이상이어야 합니다. 현재 값 " + resultPrice));
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,resultPrice}, null));
             }
         }
 
